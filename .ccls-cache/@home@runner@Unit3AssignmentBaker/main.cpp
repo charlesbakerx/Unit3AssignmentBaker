d1cc324da383d00de88e7d9f07446552aa959daf
charlesbakerx <126838351+charlesbakerx@users.noreply.github.com>
2023-09-04 20:55:38 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Unit3AssignmentBaker/main.cpp
@@ -0,0 +1,55 @@
+/*
+Charles Baker, Programming Fundamentals I: COSC-1436-57001
+IDE: Replit
+Summary: Take 4 numbers from the user and use the various arithmetic operators to do some calculations. Makes use of conditions to not allow an input of 0 for numbers 2 and 4.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // Variable Declarations
+  double number1, number2, addition, subtraction, multiplication, division;
+  int number3, number4, modulus;
+
+  // Input
+  cout << "Please enter Number 1: ";
+  cin >> number1;
+  while (true) {
+    cout << "Please enter Number 2: ";
+    cin >> number2;
+    if (number2 != 0) {
+      break;
+    } else {
+      cout << "Number can not be 0" << endl;
+    }
+  }
+  cout << "Please enter Number 3: ";
+  cin >> number3;
+  while (true) {
+    cout << "Please enter Number 4: ";
+    cin >> number4;
+    if (number4 != 0) {
+      break;
+    } else {
+      cout << "Number can not be 0" << endl;
+    }
+  }
+
+  // Processing
+  addition = number1 + number2;
+  subtraction = number1 - number2;
+  multiplication = number1 * number2;
+  division = number1 / number2;
+  modulus = number3 % number4;
+
+  // Output
+  cout << number1 << " + " << number2 << " = " << addition << endl;
+  cout << number1 << " - " << number2 << " = " << subtraction << endl;
+  cout << number1 << " * " << number2 << " = " << multiplication << endl;
+  cout << number1 << " / " << number2 << " = " << division << endl;
+  cout << number3 << " % " << number4 << " = " << modulus << endl;
+
+  return 0;
+}
---
+++ Unit3_Assignment_Plan_Baker.md
@@ -0,0 +1,21 @@
+## Unit 3 Assignment Plan : Charles Baker
+
+### Variable Declarations
+- double number1, number2, addition, subtraction, multiplication, division
+- integer number3, number4, modulus
+
+### Input
+- Prompt user for numbers 1-4 and save them accordingly.
+
+### Processing
+- Add number1 and number2 >> addition
+- Subtract number1 and number2 >> subtraction
+- Multiply number1 and number2 >> multiplication
+- Divide number1 and number2 >> division
+- Modulo on number3 and number4 >> modulus
+
+### Output
+- Output the results of all processes to the console.
+
+### Error Conditions
+- Ensure that the user can not enter 0 for number2 or number4, or catch the exception and prompt for another number.
